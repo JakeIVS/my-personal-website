f0ee2bae7916bcebf221e0d30f18d33037a744f8
Jacob Ives <jacob.w.ives@gmail.com>
2023-04-28 14:55:08 -0600

    fix javascript, resize background image, clean up HTML nesting

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const h2 = document.createElement('h2');
 h2.textContent = "Professional Experience";
-document.querySelector('div #professional-info').appendChild(h2);
+document.querySelector('body').appendChild(h2);
 const h3 = document.createElement('h3')
 h3.textContent = "Chemistry"
-body.querySelector('#professional-info').appendChild(h3);
+document.querySelector('body').appendChild(h3);
+const h3_2 = document.createElement('h3');
+h3_2.textContent = "Software Engineering";
+document.querySelector('body').appendChild(h3_2);
