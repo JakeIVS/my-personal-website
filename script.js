05ca72b07c87d635a1fed33600e94a653ca80b98
Jacob Ives <jacob.w.ives@gmail.com>
2023-04-28 14:43:59 -0600

    change background, break the javascript oopsie

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
-document.querySelector('body').appendChild(h2);
+h2.textContent = "Professional Experience";
+document.querySelector('div #professional-info').appendChild(h2);
+const h3 = document.createElement('h3')
+h3.textContent = "Chemistry"
+body.querySelector('#professional-info').appendChild(h3);
