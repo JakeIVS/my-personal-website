e1d2d58a617133395ff14253968376b44b894876
Jacob Ives <jacob.w.ives@gmail.com>
2023-04-28 16:12:28 -0600

    centers text

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 const h2 = document.createElement('h2');
 h2.textContent = "Professional Experience";
 document.querySelector('body').appendChild(h2);
-const h3 = document.createElement('h3')
-h3.textContent = "Chemistry"
-document.querySelector('body').appendChild(h3);
-const h3_2 = document.createElement('h3');
-h3_2.textContent = "Software Engineering";
-document.querySelector('body').appendChild(h3_2);
+const button1 = document.createElement('button');
+button1.textContent = "Chemistry"
+document.querySelector('body').appendChild(button1);
+const button2 = document.createElement('button');
+button2.textContent = "Software Engineering";
+document.querySelector('body').appendChild(button2);
+const p = document.createElement('p');
 
-h3.className = "button1";
-h3_2.className = "button2";
 
-// Adding mouse-over functionality
+button1.id = "button1";
+button2.id = "button2";
+
+// Adding click functionality
 
 const p2 = document.createElement('p');
 p2.textContent = "Software Engineering at Flatirons School (Denver, CO)";
 
-function chemistryShow() {
-    const p1 = document.createElement('p');
-    p1.textContent = 'BS in Chemistry, Colorado State University';
-    document.querySelector('body').addChild(p1);
+function showChemistry() {
+    const chemExp = document.getElementById('chemical-experience');
+    const seExp = document.getElementById('se-experience');
+    seExp.style.display = 'none';
+    chemExp.style.display = 'block';
 }
 
 const input1 = document.getElementsByClassName('button1');
-input1.addEventListener('click', chemistryShow);
+input1.addEventListener('click', showChemistry());
