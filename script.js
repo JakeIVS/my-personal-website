7dd046ed9ff222e4a5050ce9c0a991466f3cabb8
Jacob Ives <jacob.w.ives@gmail.com>
2023-04-28 15:22:33 -0600

    Add experience section, attempt to show on header click

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ document.querySelector('body').appendChild(h3);
 const h3_2 = document.createElement('h3');
 h3_2.textContent = "Software Engineering";
 document.querySelector('body').appendChild(h3_2);
+
+h3.className = "button1";
+h3_2.className = "button2";
+
+// Adding mouse-over functionality
+
+const p2 = document.createElement('p');
+p2.textContent = "Software Engineering at Flatirons School (Denver, CO)";
+
+function chemistryShow() {
+    const p1 = document.createElement('p');
+    p1.textContent = 'BS in Chemistry, Colorado State University';
+    document.querySelector('body').addChild(p1);
+}
+
+const input1 = document.getElementsByClassName('button1');
+input1.addEventListener('click', chemistryShow);
